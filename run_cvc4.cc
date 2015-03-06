01642f983c13b025a5262ea00a315a3b6e0e1394
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-03-06 00:13:30 -0500

    reading and writing

--- run_cvc4.cc
+++ run_cvc4.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <string>
 
 
 using namespace std;
@@ -8,6 +10,15 @@ using namespace std;
 int main() {
     FILE* program_output = popen("cvc4 smtlib_test.smt2", "r");
     
+    if (program_output == NULL) { 
+        printf("FAIL!\n"); 
+    } 
+       
+    char buffer[1028]; 
+    while (fgets(buffer, 1028, program_output) != NULL) { 
+        printf(buffer); 
+    } 
+
     pclose(program_output);
     return 0;
 }
